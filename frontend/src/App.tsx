ac660834d79504f5f62d56c1d67b9303b0297efa
Hunter Chen <hello@hunterchen.ca>
2025-01-18 22:21:58 -0700

    add image background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import DogLogo from "/dog_land.gif";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <main className='flex h-screen flex-col items-center bg-blue-300'>
-      <div className='bg-black rounded-lg'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-green-600 text-2xl'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)} className='bg-white p-2 rounded-md hover:bg-gray-200 transition-all'>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <main className="flex h-screen flex-col items-center bg-cover bg-[url('bg.svg')] justify-center mx-auto">
+      <img
+        src={DogLogo}
+        className="logo"
+        alt="Dog logo"
+        width={350}
+        height={350}
+      />
+      <h1 className="text-black text-5xl mb-4 font-semibold">Pawfect Pitch</h1>
+      <a
+        className="bg-black rounded-lg m-6 px-8 py-3 text-white text-2xl hover:bg-slate-700 transition-all"
+        href="/chat"
+      >
+        Get Started
+      </a>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
