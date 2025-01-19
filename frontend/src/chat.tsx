865ffe00f685953cdd84eab0bf389e6b1441336c
Hunter Chen <hello@hunterchen.ca>
2025-01-19 10:28:04 -0800

    new bone

--- frontend/src/chat.tsx
+++ frontend/src/chat.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import DogHome from "/dog_home.gif";
+import DogFeedback from "/dog_feedback.gif";
+import Bone from "/bone.png";
 import ArrowIcon from "/arrow-icon.png";
 import { LoadingModal } from "./loadingModal";
 import { Feedback } from "./feedback";
@@ -125,7 +127,7 @@ const Chat: React.FC = () => {
             {/* Column 1 */}
             <a href="/">
               <img
-                src={DogHome}
+                src={!focusedPresentation ? DogHome : DogFeedback}
                 className="Dog home"
                 alt="Dog home"
                 width={242}
@@ -136,7 +138,9 @@ const Chat: React.FC = () => {
             {/* Column 1 */}
             <div className="flex-1 p-4 text-left whitespace-nowrap">
               <h1 className="text-black text-[24px] mb-4 font-medium mt-[35px] ml-[200px]">
-                Let's practice a presentation today!
+                {!focusedPresentation
+                  ? "Let's practice a presentation today!"
+                  : "We've fetched your results, friend!"}
               </h1>
             </div>
           </div>
@@ -176,28 +180,21 @@ const Chat: React.FC = () => {
           <div className="flex items-start mb-3 ml-[58px]">
             <div className="flex gap-4">
               {/* Column 1 */}
-              <div className="flex flex-col p-4 text-left whitespace-nowrap">
+              <div className="flex flex-col px-4 text-left whitespace-nowrap justify-between">
                 <h1 className="text-black text-[24px] mb-4 font-medium">
                   My Dashboard
                 </h1>
-                <svg
-                  width="120"
-                  height="71"
-                  viewBox="0 0 120 71"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M87.8531 43.3747C89.6798 44.4302 91.4579 45.8884 93.0733 47.2659C97.7278 51.2352 103.526 53.8782 109.68 53.299C113.436 52.9456 115.791 52.458 117.823 48.9416C120.457 44.3831 113.925 37.6849 110.429 35.076C109.843 34.6387 105.728 32.8818 106.211 32.0454C106.959 30.7504 108.334 29.3671 109.28 28.1939C111.629 25.2826 113.284 21.7521 115.149 18.5231C116.11 16.86 118.106 14.5572 117.816 12.5017C117.344 9.15211 115.064 5.86034 112.252 4.06816C105.501 -0.233907 99.755 1.1401 94.5618 6.94014C91.8243 9.99741 89.1026 13.5182 90.8824 17.6887C91.5049 19.1473 92.3859 20.6509 92.7066 22.2113C92.8492 22.905 93.7853 23.5535 93.7103 24.2599C93.5438 25.8293 83.8121 28.032 82.2661 28.4908C74.1236 30.9072 66.2092 34.1336 57.7903 35.5354C55.7649 35.8727 53.8396 36.7692 51.9146 37.484C46.3069 39.5662 39.9246 40.201 34.0143 40.8282C32.8415 40.9526 31.583 41.1253 30.4085 41.1197C29.0956 41.1134 29.7905 40.291 29.8744 39.1236C30.181 34.8598 30.0427 32.7668 26.3135 30.0352C21.3553 26.4033 13.1012 22.9508 6.99044 25.6201C-1.01313 29.1162 2.31864 38.7897 7.99238 42.7608C10.7543 44.6939 13.8775 45.7965 16.8204 47.3304C17.6061 47.7399 24.1904 49.1609 22.7231 49.5535C14.2488 51.8211 15.3507 62.526 21.3926 67.3778C26.3105 71.3269 33.9335 66.6031 36.4827 62.1911C37.2102 60.9321 37.9567 58.7683 39.2447 58.0059C41.6571 56.5779 44.38 55.7207 47.0753 54.9995C49.435 54.3681 51.7866 53.9459 54.1827 53.7314C57.212 53.4602 60.0961 52.5832 63.1977 52.1905C68.4725 51.5227 72.6967 47.7473 77.6563 46.4202C80.1109 45.7634 82.3641 44.1689 85.0959 44.469C85.8502 44.5519 89.2268 44.295 89.7835 43.9277"
-                    stroke="black"
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                  />
-                </svg>
+                <img
+                  src={Bone}
+                  className="-rotate-12 mx-auto mb-3"
+                  alt="Bone icon"
+                  width={135}
+                  height={135}
+                />
               </div>
 
               {/* Column 2 */}
-              <div className=" ml-[240px] flex-1 p-[20px] border bg-white text-left flex flex-row justify-between items-start w-[280px] h-[130px] rounded-[12px]">
+              <div className="ml-[240px] flex-1 p-[20px] border bg-white text-left flex flex-row justify-between items-start w-[280px] h-[130px] rounded-[12px]">
                 <p className="text-black text-[16px] mb-4 font-normal text-left">
                   Total times <br />
                   practiced
